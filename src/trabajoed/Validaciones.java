8d71cb091ba26a6c590ca70065bba8a3a07c1bda
DAM124 <DAM124@IF02-02.iesmhp.local>
2018-04-26 16:15:08 +0200

    Creación de la clase validaciones.

--- src/trabajoed/Validaciones.java
+++ src/trabajoed/Validaciones.java
@@ -16,163 +16,97 @@ import java.util.regex.Pattern;
  */
 public class Validaciones {
     
-    public static String introducirNombre() {
-        Scanner sc = new Scanner(System.in);
+    public static boolean introducirNombre(String nombre) {
+        
         boolean valido = false;
-        String nombre = "";
-
-        while (!valido) {
-            System.out.println("Introducir nombre: ");
-            
-            nombre = sc.nextLine();
+        
             if (nombre.matches("[A-Z]{1}[a-z ]{2,15}")) {
                 valido = true;
-            } else {
-                System.err.println("ERROR. Nombre no válido.");
             }
-        }
-        return nombre;
+        
+        return valido;
     }
 
-    public static String introducirDireccion() {
-        Scanner sc = new Scanner(System.in);
+    public static boolean introducirDireccion(String direccion) {
+        
         boolean valido = false;
-        String direccion = "";
-
-        while (!valido) {
-            System.out.println("Introducir dirección: ");
-            direccion = sc.nextLine();
+        
             if (direccion.matches("[A-Z]{1}[a-z ]{2,30}")) {
                 valido = true;
-            } else {
-                System.err.println("ERROR. Dirección no válida.");
             }
-        }
-        return direccion;
+        
+        return valido;
     }
     
-    public static String introducirNIF() {
+    public static boolean introducirNIF(String NIF) {
 
-        Scanner sc = new Scanner(System.in);
-        String nif="";
         boolean correcto = false;
 
-        while (!correcto) {
-            System.out.println("Introducir NIF: ");
-            nif = sc.nextLine();
-            Pattern pattern = Pattern.compile("(\\d{1,8})([TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke])");
-            Matcher matcher = pattern.matcher(nif);
+        Pattern pattern = Pattern.compile("(\\d{1,8})([TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke])");
+        Matcher matcher = pattern.matcher(NIF);
 
-            if (matcher.matches()) {
-                String letra = matcher.group(2);
-                String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
-                int index = Integer.parseInt(matcher.group(1));
-                index = index % 23;
-                String reference = letras.substring(index, index + 1);
-                if (reference.equalsIgnoreCase(letra)) {
-                    correcto = true;
-                } else {
-                    correcto = false;
-                }
+        if (matcher.matches()) {
+            String letra = matcher.group(2);
+            String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+            int index = Integer.parseInt(matcher.group(1));
+            index = index % 23;
+            String reference = letras.substring(index, index + 1);
+            if (reference.equalsIgnoreCase(letra)) {
+                correcto = true;
             } else {
                 correcto = false;
             }
+        } else {
+            correcto = false;
         }
-        return nif;
+
+        return correcto;
 
     }
 
 
-    public static String introducirNumero() {
-        Scanner sc = new Scanner(System.in);
+    public static boolean introducirNumero(String tlf) {
+
         boolean valido = false;
-        String tlf = "";
-         while (!valido) {
-            System.out.println("Introducir número de tlf: ");
-            tlf = sc.nextLine();
-            if (tlf.matches("[0-9]{9}")) {
-                valido = true;
-            } else {
-                System.err.println("ERROR. Tlf no válido.");
-            }
+
+        if (tlf.matches("[0-9]{9}")) {
+            valido = true;
+        } else {
+            System.err.println("ERROR. Tlf no válido.");
+
         }
-        return tlf;
-       
+        return valido;
+
     }
     
-    public static String introducorTarjeta(){
-        Scanner sc=new Scanner(System.in);
+    public static boolean introducorTarjeta(String tarjeta){
+        
         boolean valido = false;
-        String tarjeta =" ";
-         while (!valido) {
-            System.out.println("Introducir Tarjeta de Crédito: ");
-            tarjeta = sc.nextLine();
+        
+         
             if (tarjeta.matches("[0-9][16]")) {
                 valido = true;
             } else {
                 System.err.println("ERROR. Tarjeta no válida.");
             }
-        }
-        return tarjeta;
+        
+        return valido;
     }
     
-    public static String introducirCuenta(){
-        Scanner sc=new Scanner(System.in);
+    public static boolean introducirCuenta(String cuenta){
+        
         boolean valido = false;
-        String cuenta =" ";
-         while (!valido) {
-            System.out.println("Introducir Cuenta bancaria: ");
-            cuenta = sc.nextLine();
+        
+        
             if (cuenta.matches("[0-9][20]")) {
                 valido = true;
             } else {
                 System.err.println("ERROR. Cuenta bancaria.");
             }
-        }
-        return cuenta;
+        
+        return valido;
         
     }
-    
-    public static double introducirPrecio(){
-        Scanner sc = new Scanner(System.in);
-        boolean valido = false;
-        double precio=0;
-        while(!valido){
-            try{
-                System.out.println("Introduce el precio por metro cuadrado: ");
-                precio=sc.nextDouble();
-                if((precio>0)){
-                    valido = true;
-                }else{
-                    System.err.println("ERROR. Precio no válido.");
-                }
-            }catch(InputMismatchException ex){
-                System.out.println("ERROR. El precio debe ser un número de tipo double.");
-                sc.next();
-            }
-        }
-        
-        return precio;
-    }
-
-    public static int introducirOpcion() {
-        Scanner sc = new Scanner(System.in);
-        boolean valido = false;
-        int num = 0;
-        while (!valido) {
-            try {
-                System.out.println("Introduce la opción: ");
-                num = sc.nextInt();
-                if ((num >= 1) && (num <= 7)) {
-                    valido = true;
-                } else {
-                    System.err.println("ERROR. Número de opción no válido.");
-                }
-            } catch (InputMismatchException ex) {
-                System.err.println("ERROR. No se ha introducido un número entero.");
-            }
-        }
-        return num;
-    }
-    
 }
+    
+
